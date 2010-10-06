760976b6ff1c2f5f9c2b74d69db480b7a220ebac
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2010-10-06 04:36:09 -0500

    Compute the proper architecture of each binary because we will distributed compile in a heterogenous environment.

--- configure.zsh
+++ configure.zsh
@@ -1,27 +1,26 @@
 #!/bin/zsh
 
 ME=$(basename $0)
-USAGE="usage: ${ME} [-j #] [-m] name [options]"
+USAGE="usage: ${ME} [-j #] [-m] [-n name] [options]"
 
 want_make=
 jobs=12
+NAME=$(basename ${PWD})
 
-while getopts j:m c; do
+while getopts j:mn: c; do
 	case "${c}" in
 	j )	jobs="${OPTARG}";;
 	m )	want_make=yes;;
+	n )	NAME="${OPTARG}";;
 	* )	echo "${USAGE}" >&2; exit 1;;
 	esac
 done
 shift $((OPTIND - 1))
 
-if [ $# -lt 1 ]; then
-	echo "${USAGE}" >&2
-	exit 1
+if [ $# -ge 1 ]; then
+	NAME="${1}"
+	shift
 fi
-
-NAME="${1}"
-shift
 
 CUSTOM="${0}-${NAME}"
 
@@ -29,11 +28,15 @@ if [ -x "${CUSTOM}" ]; then
 	echo "Running configure with preferred arguments"
 	. "${CUSTOM}"
 else
+	case "$(arch)" in
+	x86_64 )	CCMODE='-m64';;
+	* )		CCMODE='-m32';;
+	esac
 	echo "Running configure with standard arguments"
 	export	CCACHE_PREFIX=distcc
-	export	CC="ccache gcc -std=gnu99 -march=native"
+	export	CC="ccache gcc -std=gnu99 ${CCMODE}"
 	export	CFLAGS='-pipe -Os -D_FORTIFY_SOURCE=2'
-	export	CXX='ccache g++'
+	export	CXX="ccache g++ ${CCMODE}"
 	export	CXXFLAGS='-pipe -Os'
 fi
 #
