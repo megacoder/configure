39e5acb367ea65c37327e8a8df5363e0c9556ada
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2010-12-21 17:48:40 -0600

    Added '-v' switch to run the bootstrap tool in verbose mode by defining the "BOOTSTRAP_VERBOSE" environment variable..

--- configure.zsh
+++ configure.zsh
@@ -1,17 +1,19 @@
 #!/bin/zsh
 
 ME=$(basename $0)
-USAGE="usage: ${ME} [-j #] [-m] [-n name] [options]"
+USAGE="usage: ${ME} [-j #] [-m] [-n name] [-v] [options]"
 
+VERBOSE=""
 want_make=
 jobs=12
 NAME=$(basename ${PWD})
 
-while getopts j:mn: c; do
+while getopts j:mn:v c; do
 	case "${c}" in
 	j )	jobs="${OPTARG}";;
 	m )	want_make=yes;;
 	n )	NAME="${OPTARG}";;
+	v )	VERBOSE="yes";;
 	* )	echo "${USAGE}" >&2; exit 1;;
 	esac
 done
@@ -41,7 +43,13 @@ else
 fi
 #
 if [ ! -x ./configure ]; then
-	bootstrap
+	if [ "${VERBOSE}" ]; then
+		export BOOTSTRAP_VERBOSE=yes
+		bootstrap ${VERBOSE}
+	else
+		unset BOOTSTRAP_VERBOSE
+		bootstrap ${VERBOSE}
+	fi
 fi
 #
 ./configure								\
